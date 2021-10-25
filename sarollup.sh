427ec9c046a3a9f64effc2e5c57ef8d6754d6f3d
Chris 'Jones' Nietzold <iczerjones@gmail.com>
2021-10-25 09:40:51 -0500

    Add more logging and single bundle
    Add ethtool, smartctl, dmi, and log info and output archive for download

--- sarollup.sh
+++ sarollup.sh
@@ -1,25 +1,35 @@
-#!/bin/bash
+#!/usr/bin/bash
 # Concatenates sar logs in chronological order to create
-# a one month record
+# a one month record. Also gathers core logs and hardware
+# information
+#
+# cnietzold@scalecomputing.com
+
 
 # set finished file suffix
 descriptor=`hostname`
+
+# cleanup all arrays and temp directory
+cleanup(){
+	sync; rm -rf ${tdir}/*; rm -rf ${tdir}
+	unset sars; unset last; unset part; sync
+}
 
 # take argument for sar file location, or check for common
 # location in /var/log/sa/sar* and set as location if found
 location=$1
 if [ $# -eq 0 ]; then
-        if [ `ls /var/log/sa/sar* | wc -l` -gt 2 ]; then
-                location="/var/log/sa"
-        else
-                echo "usage: ./sarollup.sh [PATH] "; sleep 1 
-                exit 1
-        fi
+	if [ `ls /var/log/sa/sar* | wc -l` -gt 2 ]; then
+		location="/var/log/sa"
+	else
+		echo "usage: ./sarollup.sh [PATH] "; sleep 1
+		exit 1
+	fi
 fi
 
 # remove trailing / if present
 if [[ `echo ${location: -1}` == "/" ]]; then
-        location=${location%/}
+	location=${location%/}
 fi
 
 # set todays day of the month as a reference then note the
@@ -32,19 +42,43 @@ last=$(ls ${location}/sar* | wc -l)
 # behind, start with today + 1, unless today is 1, then 
 # iterate through the entirety of last month, start to finish
 if [ $now -eq 1 ]; then
-        sars=( $(seq 1 $last) )
+	sars=( $(seq 1 $last) )
 else
-        sars=( $(seq $(echo "$now + 1" | bc) $last ) )
-        sars+=( $(seq 1 $now) )
+	sars=( $(seq $(echo "$now + 1" | bc) $last ) )
+	sars+=( $(seq 1 $now) )
 fi
 
-# set a file to write out to using $descriptor as suffix
-target="sarfile_${descriptor}"
+# set a file to write out to using $descriptor as suffix;
+# create temp directory for storing log files
+tdir="/tmp/supportbundle"
+mkdir $tdir; sync
+target="${tdir}/sarfile_${descriptor}"
 
 # append each day's logs in chronological order
 for part in ${sars[@]}; do
-        if [ ${#part} -eq 1 ]; then
-                part="0${part}"
-        fi
-        cat ${location}/sar${part} >> $target
+	if [ ${#part} -eq 1 ]; then
+		part="0${part}"
+	fi
+	cat ${location}/sar${part} >> $target
 done
+
+# gather ethtool info
+ethtool -i lan0 > ${tdir}/eth_info-${descriptor}
+echo "-------------------------" >> ${tdir}/eth_info-${descriptor}
+ethtool -d lan0 >> ${tdir}/eth_info-${descriptor}
+echo "-------------------------" >> ${tdir}/eth_info-${descriptor}
+ethtool lan0 >> ${tdir}/eth_info-${descriptor}
+
+# gather smartctl drive info
+smartctl -a /dev/nvme0n1 > ${tdir}/nvme_info-${descriptor}
+
+# gather dmidecode info
+dmidecode > ${tdir}/dmi_info-${descriptor}
+
+# gather dmesg and scale logs
+tar czf ${tdir}/messages-${descriptor} /var/log/messages*
+tar czf ${tdir}/scalelogs-${descriptor}.tar.gz /var/log/scale/*
+
+# create single bundle and clean up
+sync; tar czf supportbundle-${descriptor} ${tdir}/*
+echo "All done.  ;) "; sleep 2; exit 0
